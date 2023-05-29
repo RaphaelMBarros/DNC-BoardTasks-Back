47f3d57127021fa948e9ade7de00563a8362267f
RaphaelMBarros <118280742+RaphaelMBarros@users.noreply.github.com>
2023-05-29 19:18:07 -0300

    Doc. Readme criada. E pasta src criada

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
-const swaggerFile = require('./swagger_output.json');
 const swaggerOptions = { customCssUrl: '/swagger-ui.css'};
-
-const usersRouter = require('./routes/users');
-
+const routes = require('./src/routes');
 const app = express();
 require('dotenv').config();
+/* O trecho acima são as importações */
 
+/* O trecho abaixo são as configurações do express */
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,11 +18,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => { /* #swagger.ignore = true */ res.redirect('/doc');});
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
 
-app.use('/users', usersRouter);
+/* O trecho abaixo é a rota do swagger, para documentação */
+if(process.env.NODE_ENV !== 'test') {
+    const swaggerFile = require('./swagger_output.json');
+    app.get('/', (req, res) => { /* #swagger.ignore = true */ res.redirect('/doc');});
+    app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
+}
 
+
+/* O código é onde estão as rotas da API */
+routes(app);
+
+
+/* O trecho abaixo é a inicialização do servidor */
 if (process.env.NODE_ENV !== 'test') {
     const PORT = process.env.PORT || 4000;
     app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
